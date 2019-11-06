8fd7e5371061259eb9d481ec29deb680ad8eb537
Gina Pultorak <gina.pultorak@gmail.com>
2019-11-06 08:24:26 -0800

    make initial commit react js adopt me

---
+++ src/App.js
@@ -0,0 +1,40 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement(
+    "div", 
+    {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+    ]);
+};
+
+
+const App = () => {
+  return React.createElement(
+    "div",
+    { id: "something-important" },
+    [
+      React.createElement("h1", {}, "Adopt me!"),
+      React.createElement(Pet, {
+        name: "Ellie",
+        animal: "Dog",
+        breed: "Lab"
+      }),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Butch",
+        animal: "Dog",
+        breed: "Collie"
+      })
+    ]
+  );
+};
+
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById("root")
+  );
